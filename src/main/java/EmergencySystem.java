2fbcf6bf421fc261a2f981cfba5146dbfad4ba38
Angelina <angelina.p20056@gmail.com>
2025-05-24 11:43:38 +0100

    commit

--- src/main/java/EmergencySystem.java
+++ src/main/java/EmergencySystem.java
@@ -1,12 +1,18 @@
+import dto.RailLine;
+import dto.RailStation;
 import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
+import util.AddRailLines;
 import util.FileParser;
+import util.graph.GraphGenerator;
+import util.graph.GraphObjectGenerator;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class EmergencySystem {
     public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
@@ -33,6 +39,15 @@ public class EmergencySystem {
         parser.traverse(parseDoc(railLinesPath));
         parser.traverse(parseDoc(railStationsPath));
 
+        //set rail lines
+        setRailLines(parser.getRailLines(), parser.getRailStations());
+        //generate graph objects
+        GraphObjectGenerator graphObjectGenerator = new GraphObjectGenerator(parser.getRailLines(), parser.getRailStations(), parser.getRailLinesNames());
+        graphObjectGenerator.controller();
+        //generate graph
+        GraphGenerator graphGenerator = new GraphGenerator(graphObjectGenerator);
+        graphGenerator.generateGraph(graphObjectGenerator.getStations());
+        graphGenerator.printEntireMap();
     }
 
     private static Document parseDoc(File fileKML) throws ParserConfigurationException, IOException, SAXException {
@@ -42,5 +57,10 @@ public class EmergencySystem {
         Document doc = db.parse(fileKML);
         doc.getDocumentElement().normalize();
         return doc;
+    }
+
+    private static void setRailLines(ArrayList<RailLine> railLines, ArrayList<RailStation> railStations) {
+        AddRailLines addRailLines = new AddRailLines();
+        addRailLines.addLines(railLines, railStations);
     }
 }
---
+++ src/main/java/util/graph/GraphGenerator.java
@@ -0,0 +1,160 @@
+package util.graph;
+
+import dto.Route;
+import dto.Station;
+import lombok.Getter;
+import lombok.Setter;
+import org.graphstream.graph.Edge;
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.IdAlreadyInUseException;
+import org.graphstream.graph.Node;
+import org.graphstream.graph.implementations.SingleGraph;
+import org.graphstream.ui.view.Viewer;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/*For graph generation, code was taken from
+* https://graphstream-project.org/doc/Tutorials/Getting-Started/
+* https://graphstream-project.org/doc/Tutorials/Graph-Visualisation/
+* https://stackoverflow.com/questions/67331322/show-the-names-of-nodes-and-edges-using-graphstream-in-scala
+*/
+@Getter
+@Setter
+public class GraphGenerator {
+    private Graph graph;
+    private Set<String> processedEdges;
+    private GraphObjectGenerator graphObjectGenerator;
+    private List<Station> stations;
+
+    public GraphGenerator(GraphObjectGenerator graphObjectGenerator) {
+        this.graphObjectGenerator = graphObjectGenerator;
+        setSystemProperties();
+    }
+
+    private void setSystemProperties() {
+        // Same system properties as RouteGenerator
+        System.setProperty("org.graphstream.ui", "swing");
+        System.setProperty("org.graphstream.ui.renderer", "org.graphstream.ui.j2dviewer.J2DGraphRenderer");
+        this.graph = new SingleGraph("Train Graph");
+        this.processedEdges = new HashSet<>();
+
+        // Match the exact same styling attributes as RouteGenerator
+        graph.setAttribute("ui.quality");
+        graph.setAttribute("ui.antialias");
+        graph.setAttribute("ui.stylesheet", "node { size: 10px; fill-color: #666666; text-size: 14; }" + "edge { size: 2px; fill-color: #333333; }");
+    }
+
+
+    public Graph generateGraph(List<Station> stations) {
+        this.stations = stations;
+        if (stations == null) {
+            throw new IllegalArgumentException("Stations list cannot be null");
+        }
+
+        try {
+            //clear the graph
+            graph.clear();
+            processedEdges.clear();
+
+            //add graph nodes
+            addNodes(stations);
+            //add graph edges
+            addEdges(stations);
+
+        } catch (Exception e) {
+            System.err.println("Error generating graph: " + e.getMessage());
+        }
+
+        return graph;
+    }
+
+    private void addNodes(List<Station> stations) {
+        for (Station station : stations) {
+            if (isValidStation(station)) {
+                String nodeName = station.getRailStation().getName();
+                try {
+                    Node node = graph.addNode(nodeName);
+                    //set the node label to station name
+                    node.setAttribute("ui.label", nodeName);
+                } catch (IdAlreadyInUseException e) {
+                    //if node already exists, ignore
+                    System.err.println("Node " + nodeName + " already exists");
+                }
+            }
+        }
+    }
+
+    private void addEdges(List<Station> stations) {
+        for (Station station : stations) {
+            if (!isValidStation(station)) continue;
+
+            String sourceStation = station.getRailStation().getName();
+
+            for (Route route : station.getRoutes()) {
+                try {
+                    if (isValidRoute(route)) {
+                        String destStation = route.getDestination().getName();
+                        addEdgeSafely(sourceStation, destStation);
+                    }
+                } catch (Exception e) {
+                    System.err.println("Error processing route from " + sourceStation + ": " + e.getMessage());
+                }
+            }
+        }
+    }
+
+    private boolean isValidStation(Station station) {
+        return station != null &&
+                station.getRailStation() != null &&
+                station.getRailStation().getName() != null &&
+                station.getRoutes() != null;
+    }
+
+    private boolean isValidRoute(Route route) {
+        return route != null && route.getRailLine() != null && route.getRailLine().getName() != null && route.getDestination() != null && route.getDestination().getName() != null;
+    }
+
+    private void addEdgeSafely(String sourceStation, String destStation) {
+        String edgeId = sourceStation + "--" + destStation;
+        String reverseEdgeId = destStation + "--" + sourceStation;
+
+        if (processedEdges.contains(edgeId) || processedEdges.contains(reverseEdgeId)) {
+            //if edge already exists, ignore
+            return;
+        }
+
+        try {
+            Edge edge = graph.addEdge(edgeId, sourceStation, destStation, false);
+            if (edge != null) {
+                //get weight from distance map
+                Double weight = graphObjectGenerator.getStationDistances().getOrDefault(sourceStation, new HashMap<>()).getOrDefault(destStation, 1.0);
+
+                //set length as weight attribute
+                edge.setAttribute("length", weight);
+                processedEdges.add(edgeId);
+            }
+        } catch (Exception e) {
+            System.err.println("Warning: Could not create edge between " + sourceStation + " and " + destStation);
+        }
+    }
+
+    public void printEntireMap() {
+        try {
+            //set default node and edge styles
+            graph.edges().forEach(edge -> {
+                edge.setAttribute("ui.style", "fill-color: #333333;");
+            });
+            graph.nodes().forEach(node -> {
+                node.setAttribute("ui.style", "fill-color: #666666;");
+            });
+
+            Viewer viewer = graph.display();
+            viewer.setCloseFramePolicy(Viewer.CloseFramePolicy.HIDE_ONLY);
+        } catch (Exception e) {
+            System.err.println("Error displaying graph: " + e.getMessage());
+        }
+    }
+}
