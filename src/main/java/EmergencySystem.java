20d49800391c5b13dd253750f08f30dea41f0765
Angelina <angelina.p20056@gmail.com>
2025-05-30 10:37:09 +0100

    commit

--- src/main/java/EmergencySystem.java
+++ src/main/java/EmergencySystem.java
@@ -3,7 +3,6 @@ import dto.RailStation;
 import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
 import userControl.UserControl;
-import util.AddRailLines;
 import util.FileParser;
 import util.graph.GraphGenerator;
 import util.graph.GraphObjectGenerator;
@@ -14,6 +13,19 @@ import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+
+/*Code references:
+ * https://graphstream-project.org/doc/
+ * https://www.tutorialspoint.com/java_xml/java_dom_parse_document.htm
+ * https://www.geeksforgeeks.org/java-program-to-extract-content-from-a-xml-document/
+ * https://stackoverflow.com/questions/3694380/calculating-distance-between-two-points-using-latitude-longitude
+ * https://www.geodatasource.com/resources/tutorials/how-to-calculate-the-distance-between-2-locations-using-java
+ * https://www.geeksforgeeks.org/haversine-formula-to-find-distance-between-two-points-on-a-sphere/
+ * https://graphstream-project.org/doc/Algorithms/Shortest-path/Dijkstra/
+ * https://graphstream-project.org/doc/Tutorials/Storing-retrieving-and-displaying-data-in-graphs/#an-example-using-attributes-and-the-viewer
+ * https://favtutor.com/blogs/breadth-first-search-java
+ * https://www.geeksforgeeks.org/breadth-first-search-or-bfs-for-a-graph/
+ */
 
 public class EmergencySystem {
     public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
@@ -33,7 +45,7 @@ public class EmergencySystem {
 
         if (railLinesPath == null || railStationsPath == null) {
             System.err.println("Files not found");
-            System.exit(1);
+            return;
         }
 
         FileParser parser = new FileParser();
@@ -48,6 +60,7 @@ public class EmergencySystem {
         //generate graph
         GraphGenerator graphGenerator = new GraphGenerator(graphObjectGenerator);
         graphGenerator.generateGraph(graphObjectGenerator.getStations());
+        //pass to user control
         UserControl userControl = new UserControl(graphGenerator);
         userControl.start();
     }
@@ -62,7 +75,17 @@ public class EmergencySystem {
     }
 
     private static void setRailLines(ArrayList<RailLine> railLines, ArrayList<RailStation> railStations) {
-        AddRailLines addRailLines = new AddRailLines();
-        addRailLines.addLines(railLines, railStations);
+        for (RailStation station : railStations) {
+            ArrayList<RailLine> lines = new ArrayList<>();
+            String stationName = station.getName();
+
+            for (RailLine line : railLines) {
+                //check if the name of the line contains the name of the station
+                if (line.getName() != null && line.getName().contains(stationName)) {
+                    lines.add(line);
+                }
+            }
+            station.setRailLines(lines);
+        }
     }
 }
